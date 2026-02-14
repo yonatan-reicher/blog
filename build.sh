b7c0d54076add39e3e5a804af7a33590cd765497
Jonathan Reicher <yony252525@gmail.com>
2026-02-14 09:18:55 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 
+USAGE = "USAGE: build.sh [debug]"
+
+# Calculate flags
+FLAGS = '--optimize'
+if [[ $1 = 'debug' ]]; then
+    FLAGS = '--debug'
+else
+    echo "Invalid argument $1"
+    echo $USAGE
+    exit 1
+fi
+
 echo "Building Elm application..."
-elm make src/Main.elm --output=elm.js --optimize
+elm make src/Main.elm --output=build/elm.js $FLAGS
 
 echo "Build complete! Files ready for deployment."
 echo "To test locally: python3 -m http.server 8000"
